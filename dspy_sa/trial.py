95df89cb00f01e544eaf63868690e2b4c2510592
siddharthyayavaram <68839837+siddharthyayavaram@users.noreply.github.com>
2024-07-01 21:28:38 +0530

    Update trial.py

--- dspy_sa/trial.py
+++ dspy_sa/trial.py
@@ -9,7 +9,6 @@ ib = instructblip(model_name)
 dspy.settings.configure(lm=ib)
 
 image_path = "2.png"
-print("RUNNING.....\n")
 
 p1 = ib(prompt="Describe the image in detail",image_path = image_path)
 print(p1)
